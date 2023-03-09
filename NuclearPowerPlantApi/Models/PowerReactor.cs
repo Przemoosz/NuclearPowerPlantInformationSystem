b61b772eb46af338e9c20cf506e7a242317099fe
Przemysław Szewczak <przmyslawszewczak1@gmail.com>
2023-03-09 14:01:41 +0100

    initial commit

---
+++ NuclearPowerPlantApi/Models/PowerReactor.cs
@@ -0,0 +1,14 @@
+﻿namespace NuclearPowerPlantApi.Models
+{
+	internal sealed class PowerReactor
+	{
+		public int Id { get; init; }
+		public string Name { get; init; }
+		public string Origin { get; init; }
+		public DateTime TimeOfInvention { get; init; }
+		public string Coolant { get; init; }
+		public string Moderator { get; init; }
+		public int PercentageUraniumEnrichment { get; init; }
+		public string NeutronSpectrum { get; init; }
+	}
+}
---
+++ NuclearPowerPlantApi/Program.cs
@@ -0,0 +1,32 @@
+namespace NuclearPowerPlantApi
+{
+	internal sealed class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+			
+			builder.Services.AddControllers();
+			
+			builder.Services.AddEndpointsApiExplorer();
+			builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+
+			app.MapControllers();
+
+			await app.RunAsync();
+		}
+	}
+}
